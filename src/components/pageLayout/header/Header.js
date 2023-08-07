c9c04c9e3faa1d8033dcbaf51c95ec161f2d5c85
Angelique Tromper <99403100+Angeltromper@users.noreply.github.com>
2023-08-07 14:39:27 +0200

    homepage doorlopen

--- src/components/pageLayout/header/Header.js
+++ src/components/pageLayout/header/Header.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
 import Navbar from "../navbar/Navbar";
+
 import './Header.css';
 
 function Header({headerImage, pageTitle}) {
-    const navigate= useNavigate();
 
-    const toLink = (e) => {
-        e.preventDefault();
-        navigate("/inloggen");
-    }
+
+
 
     return (
         <>
             <header className="header">
-                <div className="btn">
-                    <button type="button" onClick={toLink}><span className="btn-text">inloggen</span></button>
-                </div>
-
                 <div className="header-text">
                     <div className="header-text-one">
                         <h2>Heerlijk</h2>
@@ -36,16 +29,13 @@ function Header({headerImage, pageTitle}) {
 
             <image className="header-img-container">
                 <span>
-                    <img src={headerImage} className="header-img" alt="/"/>
+                    <img src={headerImage} className="header-img" alt= "/"/>
                 </span>
-
 
 
                 <div className="page-title">
                     <h1>{pageTitle}</h1>
                 </div>
-
-
 
                 <div className="skewer--top">
 
@@ -56,10 +46,6 @@ function Header({headerImage, pageTitle}) {
 }
 
 export default Header;
-
-
-
-
 
 
 
--- src/components/pageLayout/navbar/Navbar.js
+++ src/components/pageLayout/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {NavLink, useLocation } from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import logo from "../../../assets/logo/Logo Cannoli.png";
 import {AuthContext} from "../../../context/AuthContext";
 import './Navbar.css';
@@ -8,46 +8,34 @@ import leftColors from "./../../../assets/img.header/leftColors .png";
 
 
 function Navbar() {
-    let location = useLocation().pathname;
-    console.log(location);
+    const navigate= useNavigate();
+    const toLink = (e) => {
+        e.preventDefault();
+        navigate("/inloggen");
+    };
 
     const {isAuth} = useContext(AuthContext);
 
-    function getNavClass(location) {
-        switch (location) {
-            case "":
-                return "/nav-home";
-            case "/cannoli":
-                return "/cannoli";
-            case "/giftbox":
-                return "/giftbox";
-            case "/service":
-                return "/service";
-            case "/franchise":
-                return "/franchise";
-            case "/contact":
-                return "contact";
-            case "/bestellingen":
-                return "bestellingen";
-            default:
-                return "nav-home";
-        }
-    }
 
     return (
         <nav className="nav-container">
-                <ul className={getNavClass(location) + " nav-list-items"}>
-                    <li><NavLink to="/" end exact activeClassName="active-link">Home</NavLink></li>
-                    <li><NavLink to="/cannoli" exact activeClassName="active-link">Cannoli</NavLink></li>
-                    <li><NavLink to="/giftbox" exact activeClassName="active-link">Giftbox</NavLink></li>
+            <ul className={" nav-list-items"}>
+                <li><NavLink to="/" end exact activeClassName="active-link">Home</NavLink></li>
+                <li><NavLink to="/cannoli" exact activeClassName="active-link">Cannoli</NavLink></li>
+                <li><NavLink to="/giftbox" exact activeClassName="active-link">Giftbox</NavLink></li>
 
-                    <li><NavLink to="/logo"><figure><img src= {logo} alt="logo" className="logo"/></figure></NavLink></li>
+                <li><NavLink to="/logo"><figure><img src= {logo} alt="logo" className="logo"/></figure></NavLink></li>
 
-                    <li><NavLink to="/service" exact activeClassName="active-link">Service</NavLink></li>
-                    <li><NavLink to="/franchise" exact activeClassName="active-link">Franchise</NavLink></li>
-                    <li><NavLink to="/contact" exact activeClassName="active-link">Contact</NavLink></li>
-                    {isAuth && <li><NavLink to="/orderList" end activeClassName="active-link">Bestellingen</NavLink></li>}
-                </ul>
+                <li><NavLink to="/service" exact activeClassName="active-link">Service</NavLink></li>
+                <li><NavLink to="/franchise" exact activeClassName="active-link">Franchise</NavLink></li>
+                <li><NavLink to="/contact" exact activeClassName="active-link">Contact</NavLink></li>
+                {isAuth && <li><NavLink to="/orderList" end activeClassName="active-link">Bestellingen</NavLink></li>}
+            </ul>
+
+            <div className="btn">
+                <button type="button" onClick={toLink}><span className="btn-text">inloggen</span></button>
+            </div>
+
 
             <div className="color-container">
                 <figure><img src={rightColors} alt="right-colors" className="right"/></figure>
